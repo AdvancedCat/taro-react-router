79ba105158329c8fa886e63f113cb1d0a086a8c1
advancedcat <wshx1938@163.com>
2022-08-17 15:15:06 +0800

    add examples - custom active link

---
+++ src/pages/react-router-examples/custom-active-link/index.config.js
@@ -0,0 +1,3 @@
+export default definePageConfig({
+  navigationBarTitleText: 'Custom Active Link'
+})
---
+++ src/pages/react-router-examples/custom-active-link/index.jsx
@@ -0,0 +1,99 @@
+import * as React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  useMatch,
+  useResolvedPath,
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <h1>Custom Link Example</h1>
+
+      <p>
+        This example demonstrates how to create a custom{" "}
+        <code>&lt;Link&gt;</code> component that knows whether or not it is
+        "active" using the low-level <code>useResolvedPath()</code> and
+        <code> useMatch() </code> hooks.
+      </p>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function CustomLink({ children, to, ...props }) {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <div>
+      <Link
+        style={{ textDecoration: match ? "underline" : "none" }}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+      {match && " (active)"}
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <CustomLink to="/">Home</CustomLink>
+          </li>
+          <li>
+            <CustomLink to="/about">About</CustomLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div style={{margin: '10px 0', border: '2px solid black'}}></div>
+
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h1>Nothing to see here!</h1>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
